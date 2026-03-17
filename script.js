651b763a9e96e22dc8265a5d0d98dc7c52edbd26
SoftwareDev-EzinneO <esthyonuoha10@gmail.com>
2026-03-17 16:20:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function checkAge(age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return confirm ("Daily Plan, Weekly Plan, Monthly Plan");
+    }
+}
+let age = prompt ("WELCOME TO MTN DATA PLAN","");
+if (checkAge(312)) {
+    alert ("Daily Plan, Weekly Plan, Monthly Plan");
+} else {
+    alert ("1. N75 = 75MB");
+}
+let MTN = prompt ("DATA PLANS","");
+if (checkAge(312)) {
+    alert ("1. N75 = 75MB");
+} else {
+    alert ("1. N75 = 75MB");
+}
+let Mtn2 = prompt ("DAILY PLANS 2","");
+if (checkAge(312)) {
+    alert ("2. N100 = 100MB");
+} else {
+    alert ("2. N100 = 100MB");
+}
+let MTN3 = prompt ("DAILY PLANS 3","");
+if (checkAge(312)) {
+    alert ("3. N200 = 230MB");
+} else {
+    alert ("3. N200 = 230MB");
+}
+    
+let MTN4 = prompt ("DAILY PLANS 4","");
+if (checkAge(312)) {
+    alert ("4. N350 = 500MB");
+} else {
+    alert ("4. N350 = 500MB");
+}
+let MTN5 = prompt ("DAILY PLANS 5","");
+if (checkAge(312)) {
+    alert ("5. N500 = 1GB");
+} else {
+    alert ("5. N500 = 1GB");
+}
+let MTN6 = prompt ("DAILY PLANS 6","");
+if (checkAge(312)) {
+    alert ("6. N750 = 2.5GB");
+} else {
+    alert ("6. N750 = 2.5GB");
+}
